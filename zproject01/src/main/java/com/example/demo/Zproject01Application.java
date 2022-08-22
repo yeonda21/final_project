25cac9eb9057176dfdb8c9bed3f407e040698d50
jasonKim <kimyk96@gmail.com>
2022-08-22 09:59:57 +0900

    프로젝트 최초 공유

---
+++ zproject01/src/main/java/com/example/demo/Zproject01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zproject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zproject01Application.class, args);
+	}
+
+}
